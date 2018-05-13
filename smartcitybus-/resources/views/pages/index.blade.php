d1cbd10413cd2208d3bd7ca2976ed7515f678b36
defashi <davidfaustin74.df@gmail.com>
2018-05-13 23:23:58 +0300

    testing works perfect

--- smartcitybus-/resources/views/pages/index.blade.php
+++ smartcitybus-/resources/views/pages/index.blade.php
@@ -7,7 +7,7 @@
     <div class="row">
         <div class="col-md-12 margin-top="200px" ">
             <div class="panel panel-default">
-                <div class="panel-heading"><h4><b>Welcome To Smart City Busesssssssssssssss</b></h4></div>
+                <div class="panel-heading"><h4><b>Welcome To Smart City Bus</b></h4></div>
 
                 <div class="panel-body">
                     <p>Smart City Buses is the UWARDA project that allows a bus boarder with RFID card to swipe the RFID card in proximity with RFID card reader for trip payment in a particular city bus as well as being able to recharge his or her account with a mobile money service.</p><hr>
